7601b1236a3b0ae9b5217de001374b0d8ff01f3e
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-03-31 16:47:56 -0400

    First check-in

---
+++ create-apache-php-mysql-server.sh
@@ -0,0 +1,22 @@
+sudo dnf update
+sudo dnf install httpd
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
+sudo systemctl status httpd.service
+
+sudo dnf install php php-common
+sudo dnf install https://repo.mysql.com//mysql80-community-release-fc31-1.noarch.rpm
+
+sudo dnf install mysql-community-server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+sudo grep 'temporary password' /var/log/mysqld.log
+
+sudo mysql_secure_installation
+
+sudo dnf install php-mysqlnd php-xml php-json php-gd php-mbstring
+sudo firewall-cmd –permanent –add-service=http
+sudo firewall-cmd –permanent –add-service=https
+
+sudo systemctl reload firewalld
