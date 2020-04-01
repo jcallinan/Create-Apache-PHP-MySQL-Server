0e0e46bb4b4c41f2fe60c1fb4bed8ab23af39175
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-01 15:31:23 -0400

    Update firewall rules

--- create-apache-php-mysql-server.sh
+++ create-apache-php-mysql-server.sh
@@ -18,5 +18,7 @@ sudo mysql_secure_installation
 sudo dnf install php-mysqlnd php-xml php-json php-gd php-mbstring
 sudo firewall-cmd –permanent –add-service=http
 sudo firewall-cmd –permanent –add-service=https
-
+sudo firewall-cmd --zone=public --add-service=http
+sudo firewall-cmd --zone=public --add-service=https
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
 sudo systemctl reload firewalld
